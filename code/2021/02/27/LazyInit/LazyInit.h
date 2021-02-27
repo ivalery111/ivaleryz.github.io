08e9521347ef1baa852a5d2e3e1e25dc5fe5f038
Valery Ivanov <ivalery111@gmail.com>
2021-02-27 14:35:02 +0300

    feat(code/LazyInit): add interface

---
+++ code/2021/02/27/LazyInit/LazyInit.h
@@ -0,0 +1,23 @@
+#include <functional>
+#include <optional>
+
+namespace lazy {
+
+using std::function;
+using std::optional;
+
+
+template <typename T> class LazyInit {
+public:
+  explicit LazyInit(function<T()> init);
+
+  bool HasValue() const;
+  const T& Get() const;
+
+private:
+    function<T()> init_;
+    optional<T> value_;
+
+};
+
+} // namespace lazy
